ed1d0d9641e3a9eb10b5be0a3ab6de3e6e858bc0
vulcan-dev <vitexuhd@gmail.com>
2021-03-05 16:16:48 +0000

    Added the slash command functionality

--- src/cogs/commands/set_chn_log.py
+++ src/cogs/commands/set_chn_log.py
@@ -6,23 +6,36 @@ from utils import utils
 from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
 
+options = [
+    {
+        'name': 'channelID',
+        'description': 'ChannelID to set for all logging output',
+        'required': True,
+        'type': 7
+    }
+]
+
 class set_chn_log(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     #@commands.command(pass_context=True)
     #@commands.has_permissions(kick_members=True)
-    @commands.command(pass_context=True)
+    #@commands.command(pass_context=True)
     @commands.has_permissions(kick_members=True)
+    @cog_ext.cog_slash(name='set_chn_log', description='Set channel for logging', guild_ids=[757508676784488559], options=options)
     async def set_chn_log(self, ctx: SlashContext, chn):
         try:
-            channel = await self.bot.fetch_channel(int(chn))
-            utils.modify_settings('chn_log', chn)
-            await ctx.message.channel.send(f'Successfully set log channel to: {channel.mention}')
+            channel = await self.bot.fetch_channel(int(chn.id))
+            utils.modify_settings('chn_log', chn.id)
+            await ctx.respond()
+            #await ctx.respond()
+            #wait ctx.channel.send(f'Successfully set log channel to: {ctx.channel.mention}')
         except errors.NotFound:
-            await ctx.message.channel.send('Channel not found')
+            await ctx.respond()
+            await ctx.channel.send('Channel not found')
 
-        await ctx.message.delete()
+        #await ctx.message.delete()
 
 def setup(bot):
     bot.add_cog(set_chn_log(bot))
