4e7eeda355fae6a80f7c2ba9f30890d90d85f210
vulcan-dev <vitexuhd@gmail.com>
2021-03-05 16:31:11 +0000

    Added the slash command functionality

--- src/cogs/commands/set_chn_welcome.py
+++ src/cogs/commands/set_chn_welcome.py
@@ -1,24 +1,39 @@
-#description Sets welcome channel - Takes Channel ID
+#description Sets log channel - Takes Channel ID
 
-from discord.ext import commands
+import discord
 from discord import errors
 from utils import utils
+from discord.ext import commands
+from discord_slash import cog_ext, SlashContext
+
+options = [
+    {
+        'name': 'channelID',
+        'description': 'ChannelID to set for all logging output',
+        'required': True,
+        'type': 7
+    }
+]
 
 class set_chn_welcome(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True)
+    #@commands.command(pass_context=True)
+    #@commands.has_permissions(kick_members=True)
+    #@commands.command(pass_context=True)
     @commands.has_permissions(kick_members=True)
-    async def set_chn_welcome(self, ctx, chn):
-        try: 
-            channel = await self.bot.fetch_channel(int(chn))
-            utils.modify_settings('chn_welcome', chn)
-            await ctx.message.channel.send(f'Successfully set welcome channel to: {channel.mention}')
+    @cog_ext.cog_slash(name='set_chn_welcome', description='Set the welcome channel', guild_ids=[757508676784488559], options=options)
+    async def set_chn_welcome(self, ctx: SlashContext, chn):
+        try:
+            channel = await self.bot.fetch_channel(int(chn.id))
+            utils.modify_settings('chn_welcome', chn.id)
+            await ctx.respond()
         except errors.NotFound:
-            await ctx.message.channel.send('Channel not found')
+            await ctx.respond()
+            await ctx.channel.send('Channel not found')
 
-        await ctx.message.delete()
+        #await ctx.message.delete()
 
 def setup(bot):
     bot.add_cog(set_chn_welcome(bot))
