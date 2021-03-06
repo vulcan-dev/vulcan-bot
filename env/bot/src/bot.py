61d7e18be2411896e8205ac4131fcc9a9b759bea
vulcan-dev <vitexuhd@gmail.com>
2021-03-06 03:08:59 +0000

    Added requirements.txt

---
+++ env/bot/src/bot.py
@@ -0,0 +1,74 @@
+from enum import auto
+import logging
+import discord
+
+from discord_slash.context import SlashContext
+from utils import utils
+from discord import errors
+import time
+from discord.ext import commands
+from discord_slash import SlashCommand, SlashCommandOptionType, SlashContext
+
+bot = commands.Bot(command_prefix=utils.load_settings()['prefix'], intents=discord.Intents.all(), case_insensitive=True)
+bot.remove_command('help')
+
+slash = SlashCommand(bot, sync_commands=True)
+
+start_time = time.time()
+
+extensions = [
+    # Moderation
+    'cogs.commands.moderation.ban',
+    'cogs.commands.moderation.kick',
+    'cogs.commands.moderation.unban',
+    'cogs.commands.moderation.warn',
+
+    # Utils
+    'cogs.commands.utils.bdel',
+    'cogs.commands.utils.set_chn_log',
+    'cogs.commands.utils.set_chn_welcome',
+    'cogs.commands.utils.set_chn_rules',
+    'cogs.commands.utils.help',
+    'cogs.commands.utils.uptime',
+    'cogs.commands.utils.user_info',
+    'cogs.commands.utils.server_info',
+
+    # Events
+    'cogs.events.on_message',
+    'cogs.events.on_message_edit',
+    'cogs.events.on_message_delete',
+    'cogs.events.on_voice_state_update',
+    'cogs.events.reaction_roles',
+]
+
+@bot.event
+async def on_ready():
+    utils.logger.info(f'Logged in as {bot.user.name}')
+    try:
+        await bot.change_presence(activity=discord.Game(name="Daniel W is a shit owner"))
+        channel = await bot.fetch_channel(int(utils.load_settings()['chn_rules']))
+        await channel.purge(limit=2)
+
+        embed = discord.Embed(title='', description='', color=0xbb00ff)
+        embed.set_author(name='Rules', icon_url=bot.user.avatar_url)
+        embed.add_field(name='**Rule 1)**', value='Be disrespectful to all Members, treat them like a cunt.', inline=False)
+        embed.add_field(name='**Rule 2)**', value=f'Obviously no memes in {bot.get_channel(757508676784488561).mention}', inline=False)
+        embed.add_field(name='**Rule 3)**', value='Be a cunt', inline=False)
+        embed.set_footer(text=utils.get_time())
+
+        message = await channel.send(embed=embed)
+        utils.modify_settings('rules_id', message.id)
+    except errors.HTTPException as e:
+        utils.logger.error(f'HTTPException: {str(e)}')
+
+    await message.add_reaction('✅')
+
+try:
+    for ext in extensions:
+        bot.load_extension(ext)
+        utils.logger.info(f'Loaded extension: {ext}')
+
+    utils.logger.info(f'Finished loading extenions')
+    bot.run(utils.load_settings()['token'])
+except Exception as e:
+    utils.logger.warning(e)
---
+++ env/bot/src/cogs/events/on_voice_state_update.py
@@ -0,0 +1,52 @@
+import discord
+from discord.ext import commands
+from discord import errors, CategoryChannel
+from utils import utils
+from collections import OrderedDict
+
+class on_voice_state_update(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.guild = self.bot.get_guild(757508676784488559)
+        self.gid = 0
+
+#member - name
+    @commands.Cog.listener()
+    async def on_voice_state_update(self, member, before, after):
+        try:
+            if after.channel != None:
+                self.guild = self.bot.get_guild(member.guild.id)
+                if after.channel.id == 817474742332162049:
+                    member_role = discord.utils.get(self.guild.roles, name="Member")
+                    overwrites = {
+                        self.guild.default_role: discord.PermissionOverwrite(view_channel=False),
+                        member_role: discord.PermissionOverwrite(view_channel=True)
+                    }
+
+                    # Create Category and store it in a variable (vccat) - voice chat category
+                    new_category = await self.guild.create_category(name="ᵁˢᵉʳ ⱽᶜ'ˢ", position=2, overwrites=overwrites)
+                    vccat = discord.utils.get(self.guild.categories, id=new_category.id)
+
+                    # Create new voice channel
+                    new_channel = await self.guild.create_voice_channel(name=f'{member.display_name}\'s ᶜʰᵃⁿⁿᵉˡ', category=vccat)
+                    print(new_channel.id)
+
+                    # Set permission for new channel
+                    # Move member to that new channel
+                    await new_channel.set_permissions(member, connect=True, mute_members=True, manage_channels=True)
+                    await member.move_to(new_channel)
+
+                    # Check if the new_channel is empty and also check if there's any channels in the new category
+                    def check(x, y, z):
+                        return len(new_channel.members) == 0 or new_category.channels == 0
+
+                    await self.bot.wait_for('voice_state_update', check=check)
+                    await new_channel.delete()
+                    print('channel deleted')
+                    await new_category.delete()
+                    print('category deleted')
+        except Exception as e:
+            raise e
+    
+def setup(bot):
+    bot.add_cog(on_voice_state_update(bot))
