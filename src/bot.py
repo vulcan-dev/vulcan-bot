ce48c03b2fdcf9e56f33cbc16609647589c3c1ec
vulcan-dev <vitexuhd@gmail.com>
2021-03-08 21:42:24 +0000

    Made the rules ID constant

--- src/bot.py
+++ src/bot.py
@@ -46,18 +46,18 @@ async def on_ready():
     utils.logger.info(f'Logged in as {bot.user.name}')
     try:
         await bot.change_presence(activity=discord.Game(name="Daniel W is a shit owner"))
-        channel = await bot.fetch_channel(int(utils.load_settings()['chn_rules']))
-        await channel.purge(limit=2)
 
-        embed = discord.Embed(title='', description='', color=0xbb00ff)
-        embed.set_author(name='Rules', icon_url=bot.user.avatar_url)
-        embed.add_field(name='**Rule 1)**', value='Be disrespectful to all Members, treat them like a cunt.', inline=False)
-        embed.add_field(name='**Rule 2)**', value=f'Obviously no memes in {bot.get_channel(757508676784488561).mention}', inline=False)
-        embed.add_field(name='**Rule 3)**', value='Be a cunt', inline=False)
-        embed.set_footer(text=utils.get_time())
+        #channel = await bot.fetch_channel(int(utils.load_settings()['chn_rules']))
+        #await channel.purge(limit=2)
 
-        message = await channel.send(embed=embed)
-        utils.modify_settings('rules_id', message.id)
+        # embed = discord.Embed(title='', description='', color=0xbb00ff)
+        # embed.set_author(name='Rules', icon_url=bot.user.avatar_url)
+        # embed.add_field(name='**Rule 1)**', value='No racism', inline=False)
+        # embed.add_field(name='**Rule 3)**', value='Be a cunt', inline=False)
+        # embed.set_footer(text=utils.get_time())
+
+        # message = await channel.send(embed=embed)
+        #utils.modify_settings('rules_id', message.id)
     except errors.HTTPException as e:
         utils.logger.error(f'HTTPException: {str(e)}')
 
